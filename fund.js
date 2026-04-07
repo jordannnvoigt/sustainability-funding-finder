8f4e03d2832634119a72b3a830fd27073ee06a0d
Jordan Voigt <jordannnvoigt@gmail.com>
2026-04-07 14:19:09 -0500

    Add files via upload

---
+++ fund.js
@@ -0,0 +1,9 @@
+{
+  "name": "sustainability-funding-finder",
+  "version": "1.0.0",
+  "description": "AI-powered sustainability funding finder",
+  "private": true,
+  "engines": {
+    "node": ">=18"
+  }
+}
